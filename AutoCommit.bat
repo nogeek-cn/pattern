ea01c3d8c319b3f70d908429a46bd1378171d8f5
Darian <1934849492@qq.com>
2019-11-24 18:58:23 +0800

    2019/11/24   18:58:22.75 AutoCommit.bat by Darian

---
+++ AutoCommit.bat
@@ -0,0 +1,15 @@
+﻿:: 这条命令使用当前时间来作为 commit 的信息
+set commit_msg= %date:~0,10%   %time% AutoCommit.bat by Darian
+git status 
+git add * 
+git commit -m "%commit_msg%" 
+git pull gitee master
+git push gitee master
+git pull github master
+git push github master
+:: git pull origin master
+:: git push origin master 
+:: make a pause 这条命令用来等待
+:: pause
+:: Exit 用来推出当前命令
+Exit
